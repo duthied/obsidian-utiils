3f138a423f24ca3a8eaa76ba6af47c3bbdae6aca
Devlon Duthie <duthied@gmail.com>
2021-03-04 16:20:06 -0500

    Adds mindfulness link for March

--- obs-daily.rb
+++ obs-daily.rb
@@ -58,8 +58,10 @@ journal_template = <<~JOURNAL
   [[the radar]]
   > Review at the start of the day to build my stand up post
 
+  [March's Mindfulness Calendar](https://www.actionforhappiness.org/media/980902/march_2021.jpg)
+
   ### #{daily_razor}
-  
+
   ### Strategy items:
   1.
   2.
